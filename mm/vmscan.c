998b06d8925956cff566f40f9a703b2f23fd8d71
hadidjapri <hadi_250493@yahoo.com>
2013-09-10 18:37:15 +0700

    swappiness: add tuneable parameter for swappiness when using/not using zRAM in kconfig entry

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -153,7 +153,13 @@ struct mem_cgroup_zone {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 90;
+
+#ifdef CONFIG_SWAPPINESS_ZRAM
+int vm_swappiness = CONFIG_SWAPPINESS_ZRAM;
+#else
+int vm_swappiness = CONFIG_SWAPPINESS;
+#endif
+
 long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
